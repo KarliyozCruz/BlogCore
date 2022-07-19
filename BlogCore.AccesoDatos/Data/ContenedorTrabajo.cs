13bf917f6079f07156dc365563060bd922859ef1
Carlos Eduardo Cruz Gutierrez <carlos-cruz92@hotmail.com>
2022-07-19 14:10:19 -0500

    Agregar archivos de proyecto.

---
+++ BlogCore.AccesoDatos/Data/ContenedorTrabajo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BlogCore.AccesoDatos.Data.Repository;
+
+namespace BlogCore.AccesoDatos.Data
+{
+    public class ContenedorTrabajo : IContenedorTrabajo
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ContenedorTrabajo(ApplicationDbContext db)
+        {
+            _db = db;
+            Categoria = new CategoriaRepository(_db);
+        }
+        public ICategoriaRepository Categoria { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
